1582d5544db782d1a8c4f0af26af2d1a2f728e80
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2023-12-24 17:23:39 +0600

    Update app.js

--- basic-calculator/assets/scripts/app.js
+++ basic-calculator/assets/scripts/app.js
@@ -60,10 +60,28 @@ function multiply() {
 
 function divide() {
     calculateResult('DIVIDE');
+} function calculate(operation) {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let operator;
+    if (operation === 'ADD') {
+        currentResult += enteredNumber;
+        operator = '+';
+    } else if (operation === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        operator = '-';
+    } else if (operation === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        operator = '*';
+    } else {
+        currentResult /= enteredNumber;
+        operator = '/';
+    }
+    createAndWriteOutput(operator, initialResult, enteredNumber);
+    writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
-
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
