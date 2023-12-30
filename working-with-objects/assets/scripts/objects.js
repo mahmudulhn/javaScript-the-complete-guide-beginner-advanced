63f34f08f129ca565a947d62ed8f9ff4b9d7ee05
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2023-12-30 14:48:42 +0600

    Update objects.js

--- working-with-objects/assets/scripts/objects.js
+++ working-with-objects/assets/scripts/objects.js
@@ -3,6 +3,35 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+const renderMovies = (filter = '') => {
+    const moviesList = document.getElementById('movie-list');
+
+
+    if (movies.length === 0) {
+        moviesList.classList.remove('visible');
+        return;
+    } else {
+        moviesList.classList.add('visible');
+    }
+    moviesList.innerHTML = '';
+
+    const filteredMovies = !filter
+        ? movies
+        : movies.filter(movie => movie.info.title.includes(filter));
+
+    filteredMovies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + ' - ';
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + `${key}: ${movie.info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
+        moviesList.append(movieEl);
+    });
+};
+
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -25,7 +54,13 @@ const addMovieHandler = () => {
     };
 
     movies.push(newMovie);
-    console.log(movies);
+    renderMovies();
+};
+
+const searchMoviesHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMoviesHandler);
