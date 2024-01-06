af9398032ac4a3daf594b9bb3d913faf570d057f
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-01-06 23:40:00 +0600

    Done

--- mini-shop/assets/scripts/app.js
+++ mini-shop/assets/scripts/app.js
@@ -1,19 +1,3 @@
-// const products = [
-//     {
-//         title: 'A Pillow',
-//         imageUrl: '',
-//         price: 19.99,
-//         description: 'A soft pillow!'
-//     },
-//     {
-//         title: 'A Carpet',
-//         imageUrl:
-//             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX8svBt7ZCRKY-4k7Ca5ML-hH4ufag4RaGZPyLl4ajOwNXVNdceeeFGpnCswUHhdmxAkw&usqp=CAU',
-//         price: 89.99,
-//         description: 'A carpet'
-//     }
-// ];
-
 class Product {
     // title = '';
     // imageUrl;
@@ -187,48 +171,6 @@ class ProductList extends Component {
         // renderHook.append(prodList);
     }
 }
-
-// const productList = {
-//     products: [
-//         new Product(
-//             'A Pillow',
-//             'https://atlas-content-cdn.pixelsquid.com/stock-images/throw-pillow-AERZO6D-600.jpg',
-//             19.99,
-//             'A soft pillow!'
-//         ),
-//         new Product(
-//             'A Carpet',
-//             'https://static.vecteezy.com/system/resources/thumbnails/009/378/433/small/a-3d-rendering-image-of-product-display-on-white-fur-png.png',
-//             89.99,
-//             'A carpet'
-//         )
-//     ],
-
-//     render() {
-//         const renderHook = document.getElementById('app');
-//         const prodList = document.createElement('ul');
-//         prodList.className = 'product-list';
-//         for (const prod of this.products) {
-//             const prodEl = document.createElement('li');
-//             prodEl.className = 'product-item';
-//             prodEl.innerHTML = `
-//             <div>
-//                 <img src="${prod.imageUrl}" alt="${prod.title}">
-//                 <div class="product-item__content">
-//                     <h2>${prod.title}</h2>
-//                     <h3>\$${prod.price}</h3>
-//                     <p>${prod.description}</p>
-//                     <button> Add to Cart</button>
-//                 </div>
-//             </div>
-//             `
-//             prodList.append(prodEl);
-//         }
-//         renderHook.append(prodList);
-//     }
-// };
-
-// productList.render();
 
 class Shop extends Component {
     constructor() {
