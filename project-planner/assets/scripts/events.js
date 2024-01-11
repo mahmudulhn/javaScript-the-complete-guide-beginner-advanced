a63418f9ccee528f0d56aadee170f786f297544c
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-01-11 22:24:57 +0600

    14 - Working with Events

---
+++ project-planner/assets/scripts/events.js
@@ -0,0 +1,17 @@
+// const button = document.querySelector('button');
+
+// const buttonClickHandler = event => {
+//     console.log(event);
+// };
+
+// const anotherButtonClickHandler = () => {
+//     console.log('clicked');
+// }
+
+// // button.onclick = buttonClickHandler;
+
+// button.addEventListener('click', buttonClickHandler)
+
+// setTimeout(() => {
+//     button.removeEventListener('click', buttonClickHandler)
+// }, 2000);
