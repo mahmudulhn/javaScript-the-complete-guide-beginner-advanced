21365fd3ab6e639bb07aa8d7079f5ae6d8e8e7df
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-01-30 19:16:38 +0600

    share-my-place backend

---
+++ node-js-intro/routes/location.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/add-location', (req, res, next) => {
+
+});
+
+router.get('/location', (req, res, next) => {
+
+});
+
+
