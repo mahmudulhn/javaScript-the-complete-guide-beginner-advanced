ffa2da8dd675633e64f0089c460b90aa7746cf60
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-02-14 23:43:12 +0600

    done

--- ts-example/app.js
+++ ts-example/app.js
@@ -1,26 +1,64 @@
-var num1Input = document.getElementById('num1');
-var num2Input = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
+"use strict";
+// class User {
+//   name: string;
+//   private age: number;
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    print() {
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor(name, age, permissions) {
+        super(name, age);
+        this.permissions = permissions;
+    }
+}
+const user = new User('Max', 30);
+console.log(user.name);
+const num1Input = document.getElementById('num1');
+const num2Input = document.getElementById('num2');
+const buttonElement = document.querySelector('button');
 function add(a, b) {
     return a + b;
 }
-function printResult(result) {
-    console.log(result);
+var OutputMode;
+(function (OutputMode) {
+    OutputMode[OutputMode["CONSOLE"] = 0] = "CONSOLE";
+    OutputMode[OutputMode["ALERT"] = 1] = "ALERT";
+})(OutputMode || (OutputMode = {}));
+function printResult(result, printMode) {
+    if (printMode === OutputMode.CONSOLE) {
+        console.log(result);
+    }
+    else if (printMode === OutputMode.ALERT) {
+        alert(result);
+    }
 }
-// const result = add(5, 3);
-// console.log(result);
-var results = [];
-buttonElement.addEventListener('click', function () {
-    var num1 = +num1Input.value;
-    var num2 = +num2Input.value;
-    var result = add(num1, num2);
-    var resultContainer = {
+const results = [];
+const names = ['Max'];
+buttonElement.addEventListener('click', () => {
+    const num1 = +num1Input.value;
+    const num2 = +num2Input.value;
+    const result = add(num1, num2);
+    const resultContainer = {
         res: result,
-        print: function () {
+        print() {
             console.log(this.res);
-        },
+        }
     };
     results.push(resultContainer);
-    // printResult(results);
-    results[0].print();
+    // results.push(5);
+    // results[0].print();
+    printResult(result, OutputMode.CONSOLE);
+    printResult(result, OutputMode.ALERT);
+    // printResult(result, 'window');
 });
+function logAndEcho(val) {
+    console.log(val);
+    return val;
+}
+logAndEcho('Hi there!').split(' ');
--- ts-example/app.ts
+++ ts-example/app.ts
@@ -1,26 +1,74 @@
+// class User {
+//   name: string;
+//   private age: number;
+
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+interface Greetable {
+  name: string;
+}
+
+interface Printable {
+  print(): void;
+}
+
+class User implements Greetable, Printable {
+  constructor(public name: string, private age: number) {}
+
+  print() {
+    console.log(this.name);
+  }
+}
+
+class Admin extends User {
+  constructor(name: string, age: number, private permissions: string[]) {
+    super(name, age);
+  }
+}
+
+const user = new User('Max', 30);
+console.log(user.name);
+
 const num1Input = document.getElementById('num1') as HTMLInputElement;
-const num2Input = document.getElementById('num2') as HTMLInputElement;
-const buttonElement = document.querySelector('button');
+const num2Input = <HTMLInputElement>document.getElementById('num2');
+const buttonElement = document.querySelector('button')!;
 
 function add(a: number, b: number) {
   return a + b;
 }
 
-function printResult(result, printMode: 'console' | 'alert') {
-  if (printMode === 'console') {
+type PrintMode = 'console' | 'alert';
+enum OutputMode {
+  CONSOLE,
+  ALERT
+}
+
+function printResult(result: string | number, printMode: OutputMode) {
+  if (printMode === OutputMode.CONSOLE) {
     console.log(result);
-  } else {
+  } else if (printMode === OutputMode.ALERT) {
     alert(result);
   }
 }
 
 // const result = add(5, 3);
+// let isDone = false;
 
-// console.log(result);
+// printResult(result);
 
-type CalculationResults = { res: number; print: () => void }[];
+interface CalculationContainer {
+  res: number;
+  print(): void;
+}
 
-let results: CalculationResults = [];
+type CalculationResults = CalculationContainer[];
+
+const results: Array<CalculationContainer> = [];
+const names = ['Max'];
 
 buttonElement.addEventListener('click', () => {
   const num1 = +num1Input.value;
@@ -30,12 +78,19 @@ buttonElement.addEventListener('click', () => {
     res: result,
     print() {
       console.log(this.res);
-    },
+    }
   };
   results.push(resultContainer);
-  // printResult(results);
+  // results.push(5);
   // results[0].print();
-  // printResult(results);
-  printResult(result, 'console');
-  printResult(result, 'alert');
+  printResult(result, OutputMode.CONSOLE);
+  printResult(result, OutputMode.ALERT);
+  // printResult(result, 'window');
 });
+
+function logAndEcho<T>(val: T) {
+  console.log(val);
+  return val;
+}
+
+logAndEcho<string>('Hi there!').split(' ');
