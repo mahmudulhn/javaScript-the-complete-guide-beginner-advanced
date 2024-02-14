9910ea24d2c8b01dfea6b3cec04755ea00e721df
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-02-14 21:53:50 +0600

    update

--- ts-example/app.ts
+++ ts-example/app.ts
@@ -1,7 +1,41 @@
+const num1Input = document.getElementById('num1') as HTMLInputElement;
+const num2Input = document.getElementById('num2') as HTMLInputElement;
+const buttonElement = document.querySelector('button');
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-const result = add(5, 3);
+function printResult(result, printMode: 'console' | 'alert') {
+  if (printMode === 'console') {
+    console.log(result);
+  } else {
+    alert(result);
+  }
+}
 
-console.log(result);
+// const result = add(5, 3);
+
+// console.log(result);
+
+type CalculationResults = { res: number; print: () => void }[];
+
+let results: CalculationResults = [];
+
+buttonElement.addEventListener('click', () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = add(num1, num2);
+  const resultContainer = {
+    res: result,
+    print() {
+      console.log(this.res);
+    },
+  };
+  results.push(resultContainer);
+  // printResult(results);
+  // results[0].print();
+  // printResult(results);
+  printResult(result, 'console');
+  printResult(result, 'alert');
+});
