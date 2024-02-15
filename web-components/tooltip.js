5d6a08a52f6e6e316d9fd6d8dea80cba0459db86
Mahmudul Hasan Nihad <56515649+mahmudulhn@users.noreply.github.com>
2024-02-16 04:26:54 +0600

    Friday 4-26 am 16th February 2024

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -2,24 +2,41 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
+        this._tooltipText = 'Dummy tooltip text';
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <style>
+            div {
+                background-color: black;
+                color: white;
+                position: absolute;
+                z-index: 10;
+            }
+        </style>
+        <slot>Some default</slot>
+        <span> (?)</span>
+        `;
     }
 
     connectedCallback() {
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        const tooltipIcon = this.shadowRoot.querySelector('span');
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.appendChild(tooltipIcon);
+        this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
-        this.appendChild(this._tooltipContainer);
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
-        this.removeChild(this._tooltipContainer);
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
